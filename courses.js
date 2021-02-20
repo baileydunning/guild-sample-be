46450033d13131c5dc532a9def110ae2228a3640
Bailey Dunning <bwdunning@gmail.com>
2021-02-20 08:16:03 -0700

    Set up GET request for all courses

--- courses.js
+++ courses.js
@@ -1,4 +1,4 @@
-export const courses = {
+const courses = {
   data: [
     {
       id: 1,
--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require('express')
 const cors = require('cors')
+const students = require('./students')
+const courses = require('./courses')
 
 const app = express()
 
@@ -8,7 +10,14 @@ app.use(express.json())
 app.use(cors())
 
 app.locals.title = 'Course Catalog Express Server'
+app.locals.students = students.data
+app.locals.courses = courses.data
 
 app.listen(app.get('port'), () => {
   console.log(`${app.locals.title} is running on http://localhost:${app.get('port')}.`);
+})
+
+app.get('/courses', (req, res) => {
+  const allCourses = app.locals.courses
+  res.json({ allCourses })
 })
