06a94bfd8396a5dc635bcd8639c333d390dc64ae
Bailey Dunning <bwdunning@gmail.com>
2021-02-20 08:12:03 -0700

    Add local app data

---
+++ students.js
@@ -0,0 +1,31 @@
+const students = {
+  data: [
+    {
+      id: 1,
+      name: 'Array Romano',
+      email: 'rayromano@hotmail.com'
+    },
+    {
+      id: 2,
+      name: 'Codo Baggins',
+      email: 'codobaggins@lotr.io'
+    },
+    {
+      id: 3,
+      name: 'Debugs Bunny',
+      email: 'whatsup@doc.net'
+    },
+    {
+      id: 4,
+      name: 'NaNcy Pelosi',
+      email: 'NaN@pelosi.org'
+    },
+    {
+      id: 5,
+      name: 'Son Herogoku',
+      email: 'goku@dragonballz.com'
+    }
+  ]
+}
+
+module.exports = students
